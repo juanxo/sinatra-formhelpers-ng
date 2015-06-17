a7891775d24bbea2264fb376d74ac958ec8db613
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2015-06-18 01:28:50 +0200

    Add some tests

--- spec/form_helpers_spec.rb
+++ spec/form_helpers_spec.rb
@@ -3,10 +3,41 @@ require 'sinatra/form_helpers'
 require 'spec_helper'
 
 RSpec.describe Sinatra::FormHelpers do
-  let(:helpers) { Class.new { extend Sinatra::FormHelpers} }
+  let(:fh) { Class.new { extend Sinatra::FormHelpers} }
 
-  it "can create HTML tags" do
-    expected = '<a href="/some/path">Home</a>'
-    expect(helpers.tag(:a, "Home", href: '/some/path')).to eql(expected)
+  describe "#tag" do
+    it "creates HTML tags" do
+      expected = '<a href="/some/path">Home</a>'
+      expect(fh.tag(:a, "Home", href: '/some/path')).to eql(expected)
+    end
+  end
+
+  describe "#hash_to_html_attrs" do
+    it "creates HTML attribute strings from a hash" do
+      expected = 'href="/some/path" id="some-id"'
+      expect(fh.hash_to_html_attrs(href: '/some/path', id: 'some-id')).to eql(expected)
+    end
+
+    it "skips empty attributes" do
+      expected = 'href="/some/path"'
+      expect(fh.hash_to_html_attrs(href: '/some/path', id: nil)).to eql(expected)
+    end
+  end
+
+  describe "#link" do
+    it "creates an anchor tag" do
+      expected = '<a href="http://google.com/">http://google.com/</a>'
+      expect(fh.link("http://google.com/")).to eql(expected)
+    end
+
+    it "accepts custom anchor tag content" do
+      expected = '<a href="http://google.com/">Google</a>'
+      expect(fh.link("Google", "http://google.com/")).to eql(expected)
+    end
+
+    it "adds additional attributes" do
+      expected = '<a class="link" href="http://google.com/">Google</a>'
+      expect(fh.link("Google", "http://google.com/", class: "link")).to eql(expected)
+    end
   end
 end
